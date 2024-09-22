0f45ecf81379f6cea831c6c1fb142977543c5415
Liam Pewton <liampewton@Liams-MacBook-Air.local>
2024-09-22 20:52:22 +0100

    General code cleanup

--- assets/script.js
+++ assets/script.js
@@ -7,6 +7,37 @@ const getRandom = () => {
     return Math.floor(Math.random() * 12);
 };
 
+const updateBtns = () => {
+    var buttons = document.querySelectorAll('.directionBtn');
+    buttons.forEach(function (button) {
+        button.disabled = false;
+    });
+
+    if (locRow === 0) {
+        document.getElementById('u-btn').disabled = true;
+        document.getElementById('ur-btn').disabled = true;
+        document.getElementById('ul-btn').disabled = true;
+    };
+
+    if (locRow === 11) {
+        document.getElementById('d-btn').disabled = true;
+        document.getElementById('dr-btn').disabled = true;
+        document.getElementById('dl-btn').disabled = true;
+    };
+
+    if (locCell === 0) {
+        document.getElementById('l-btn').disabled = true;
+        document.getElementById('dl-btn').disabled = true;
+        document.getElementById('ul-btn').disabled = true;
+    };
+
+    if (locCell === 11) {
+        document.getElementById('r-btn').disabled = true;
+        document.getElementById('dr-btn').disabled = true;
+        document.getElementById('ur-btn').disabled = true;
+    };
+};
+
 const updateLoc = () => {
     currentLoc = locRow.toString() + '.' + locCell.toString();
     revealNewLoc();
@@ -18,110 +49,56 @@ const revealNewLoc = () => {
     revealedCell.innerHTML = 'O'
 };
 
-const updateBtns = () => {
-    var uBtn = document.getElementById('u-btn');
-    var dBtn = document.getElementById('d-btn');
-    var rBtn = document.getElementById('r-btn');
-    var lBtn = document.getElementById('l-btn');
-    var urBtn = document.getElementById('ur-btn');
-    var ulBtn = document.getElementById('ul-btn');
-    var drBtn = document.getElementById('dr-btn');
-    var dlBtn = document.getElementById('dl-btn');
-
-    if (locRow < 1) {
-        uBtn.disabled = true;
-        urBtn.disabled = true;
-        ulBtn.disabled = true;
-    } else {
-        uBtn.disabled = false;
-        urBtn.disabled = false;
-        ulBtn.disabled = false;
-    }
-    if (locRow > 10) {
-        dBtn.disabled = true;
-        drBtn.disabled = true;
-        dlBtn.disabled = true;
-    } else {
-        dBtn.disabled = false;
-        drBtn.disabled = false;
-        dlBtn.disabled = false;
-    }
-
-    if (locCell < 1) {
-        lBtn.disabled = true;
-        ulBtn.disabled = true;
-        dlBtn.disabled = true;
-    } else {
-        lBtn.disabled = false;
-        ulBtn.disabled = false;
-        dlBtn.disabled = false;
-    }
-
-    if (locCell > 10) {
-        rBtn.disabled = true;
-        urBtn.disabled = true;
-        drBtn.disabled = true;
-    } else {
-        rBtn.disabled = false;
-        urBtn.disabled = false;
-        drBtn.disabled = false;
-    }
-}
-
-updateBtns();
-
-var centerCell = 'cell-' + getRandom() + '.' + getRandom();
-
-for (var i = 0; i < 12; i++) {
-    var row = grid.insertRow();
-    row.setAttribute('id', `row-${i}`);
-    for (var j = 0; j < 12; j++) {
-        var cell = row.insertCell();
-        cell.setAttribute('id', `cell-${i}.${j}`);
-        cell.classList = 'px-3';
-        if (cell.id === centerCell) {
-            var cellText = document.createTextNode('G');
-        } else {
-            var cellText = document.createTextNode('x');
-        }
-        cell.append(cellText);
+const createGrid = () => {
+    var groundCell = 'cell-' + getRandom() + '.' + getRandom();
+    for (var i = 0; i < 12; i++) {
+        var row = grid.insertRow();
+        row.setAttribute('id', `row-${i}`);
+        for (var j = 0; j < 12; j++) {
+            var cell = row.insertCell();
+            cell.setAttribute('id', `cell-${i}.${j}`);
+            cell.classList = 'px-3';
+            if (cell.id === groundCell) {
+                var cellText = document.createTextNode('G');
+            } else {
+                var cellText = document.createTextNode('x');
+            }
+            cell.append(cellText);
+        };
+        var initialCell = document.getElementById('cell-0.0');
+        initialCell.innerHTML = 'O';
     };
 };
 
-var initialCell = document.getElementById('cell-0.0');
-initialCell.innerHTML = 'O';
-
-const rBtn = document.getElementById('r-btn');
-
 $('#u-btn').on('click', function () {
-    locRow -= 1;
     updateLoc();
+    locRow -= 1;
 });
 
 $('#d-btn').on('click', function () {
     locRow += 1;
     updateLoc();
-    });
+});
 
 $('#r-btn').on('click', function () {
     locCell += 1;
     updateLoc();
-    });
+});
 
 $('#l-btn').on('click', function () {
     locCell -= 1;
     updateLoc();
-   });
+});
 
 $('#ur-btn').on('click', function () {
     locRow -= 1;
-    locCell += 1;    
+    locCell += 1;
     updateLoc();
-   });
+});
 
 $('#ul-btn').on('click', function () {
     locRow -= 1;
-    locCell -= 1;    
+    locCell -= 1;
     updateLoc();
 });
 
@@ -136,3 +113,6 @@ $('#dl-btn').on('click', function () {
     locCell -= 1;
     updateLoc();
 });
+
+createGrid();
+updateBtns();
