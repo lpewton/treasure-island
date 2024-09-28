81a0ab94d3d01fa18daf7b95c8446203c65e01e8
Liam Pewton <liampewton@Liams-MacBook-Air.local>
2024-09-28 18:44:34 +0100

    Add and show probabilities for user

--- assets/script.js
+++ assets/script.js
@@ -6,15 +6,16 @@ var score = 0;
 var level = 1;
 var difficulty = 3;
 var limit = level * 6;
+var cellVals = {};
 
 // Functions
-const getRandom = () => {
-    return Math.floor(Math.random() * difficulty);
+const getRandom = v => {
+    return Math.floor(Math.random() * v);
 };
 
 const createGroundCell = () => {
     do {
-        groundCell = 'cell-' + getRandom() + '.' + getRandom();
+        groundCell = 'cell-' + getRandom(difficulty) + '.' + getRandom(difficulty);
     } while (groundCell === 'cell-0.0')
     return groundCell;
 };
@@ -22,15 +23,25 @@ const createGroundCell = () => {
 const createGrid = () => {
     grid.innerHTML = '';
     var groundCell = createGroundCell();
+    var groundRow = parseInt(groundCell[5]);
+    var groundCol = parseInt(groundCell[7]);
     var tbody = document.createElement('tbody');
+    currentLoc = '0.0';
+    locRow = 0;
+    locCell = 0;
     tbody.classList = 'p-3';
     grid.appendChild(tbody);
     for (var i = 0; i < difficulty; i++) {
         var row = grid.insertRow();
         row.setAttribute('id', `row-${i}`);
         for (var j = 0; j < difficulty; j++) {
+            var rowText = Math.abs(i - groundRow);
+            var colText = Math.abs(j - groundCol);
+            var textSum = rowText + colText;
+            var text = getRandom(textSum) + 1;
             var cell = row.insertCell();
             cell.setAttribute('id', `cell-${i}.${j}`);
+            cellVals[cell.id] = text;
             cell.classList = 'px-2 rounded-circle';
             // if (cell.id === groundCell) {
             //     var cellText = document.createTextNode('G');
@@ -40,14 +51,10 @@ const createGrid = () => {
             cell.append(cellText);
         };
     };
+
+    revealNewLoc();
     var tbody = document.getElementsByTagName('tbody');
     tbody.classList = 'p-3';
-    var initialCell = document.getElementById('cell-0.0');
-    initialCell.innerHTML = '<i class="fa-solid fa-sailboat"></i>';
-    initialCell.classList = 'bg-success text-center rounded-circle';
-    currentLoc = '0.0';
-    locRow = 0;
-    locCell = 0;
     document.getElementById('level').innerHTML = level;
     document.getElementById('limit').innerHTML = limit;
 };
@@ -100,10 +107,9 @@ const levelCleared = () => {
     document.getElementById('levelpassed').classList = 'bg-dark d-block vh-100 d-flex justify-content-center align-items-center';
 
     setTimeout(() => {
-    document.getElementById('main-div').classList = 'container';
-    document.getElementById('levelpassed').classList = 'd-none';
+        document.getElementById('main-div').classList = 'container';
+        document.getElementById('levelpassed').classList = 'd-none';
     }, 1500);
-
     createGrid();
 };
 
@@ -121,6 +127,49 @@ const revealNewLoc = () => {
     var revealedCell = document.getElementById(`cell-${currentLoc}`);
     revealedCell.innerHTML = '<i class="fa-solid fa-sailboat"></i>';
     revealedCell.classList = 'px-1 bg-success rounded-circle';
+
+    if (document.getElementById(`cell-${locRow}.${locCell + 1}`)) {
+        rCellText = `cell-${locRow}.${locCell + 1}`;
+        var rCell = document.getElementById(rCellText);
+        rCell.innerHTML = cellVals[rCellText];
+    };
+    if (document.getElementById(`cell-${locRow}.${locCell - 1}`)) {
+        lCellText = `cell-${locRow}.${locCell - 1}`;
+        var lCell = document.getElementById(lCellText);
+        lCell.innerHTML = cellVals[lCellText];
+    };
+    if (document.getElementById(`cell-${locRow + 1}.${locCell}`)) {
+        uCellText = `cell-${locRow + 1}.${locCell}`;
+        var uCell = document.getElementById(uCellText);
+        uCell.innerHTML = cellVals[uCellText];
+    };
+    if (document.getElementById(`cell-${locRow - 1}.${locCell}`)) {
+        dCellText = `cell-${locRow - 1}.${locCell}`;
+        var dCell = document.getElementById(dCellText);
+        dCell.innerHTML = cellVals[dCellText];
+    };
+
+    if (document.getElementById(`cell-${locRow - 1}.${locCell - 1}`)) {
+        ulCellText = `cell-${locRow - 1}.${locCell - 1}`;
+        var ulCell = document.getElementById(ulCellText);
+        ulCell.innerHTML = cellVals[ulCellText];
+    };
+    if (document.getElementById(`cell-${locRow - 1}.${locCell + 1}`)) {
+        urCellText = `cell-${locRow - 1}.${locCell + 1}`;
+        var urCell = document.getElementById(urCellText);
+        urCell.innerHTML = cellVals[urCellText];
+    };
+    if (document.getElementById(`cell-${locRow + 1}.${locCell - 1}`)) {
+        dlCellText = `cell-${locRow - 1 * -1}.${locCell - 1}`;
+        var dlCell = document.getElementById(dlCellText);
+        dlCell.innerHTML = cellVals[dlCellText];
+    };
+    if (document.getElementById(`cell-${locRow + 1}.${locCell + 1}`)) {
+        drCellText = `cell-${locRow - 1 * -1}.${locCell + 1}`;
+        var drCell = document.getElementById(drCellText);
+        drCell.innerHTML = cellVals[drCellText];
+    };
+
     if (score === (limit - 1)) {
         restartGame();
     } else {
@@ -133,14 +182,10 @@ const revealNewLoc = () => {
 };
 
 const updateLoc = () => {
-    try {
     resetOldLoc();
     currentLoc = locRow.toString() + '.' + locCell.toString();
     revealNewLoc();
     updateBtns();
-    } catch (error) {
-        console.log(oldCell)
-    }
 };
 
 // Directions and Buttons
